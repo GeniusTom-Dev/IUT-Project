0a8ebda0df335cfd8f754520b78c39eb664bdfff
GeniusTom <tomeven13@gmail.com>
2025-01-03 00:21:12 +0100

    push

--- Assets/BUT Project/Scripts/Personal/PlayerInteraction.cs
+++ Assets/BUT Project/Scripts/Personal/PlayerInteraction.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class PlayerInteraction : MonoBehaviour
 {
+
+    [Header("Annimation")]
+    [SerializeField]
+    Animator chestAnnimator;
+
     GameObject startTeleporteur;
     GameObject endTeleporteur;
+    GameObject chest;
     GameObject player;
+
 
     void Awake()
     {
         startTeleporteur = GameObject.Find("StartTeleporteur");
         endTeleporteur = GameObject.Find("EndTeleporteur");
+        chest = GameObject.Find("Chest");
         player = GameObject.Find("character");
 
     }
@@ -22,6 +30,17 @@ public class PlayerInteraction : MonoBehaviour
         if(distance < 0.5)
         {
             player.transform.position = endTeleporteur.transform.position;
+        }
+    }
+    public void openChest()
+    {
+        if (chestAnnimator?.GetBool("isOpen") == true) return;
+
+        float distance = Vector3.Distance(chest.transform.position, player.transform.position);
+        
+        if (distance < 2)
+        {
+            chestAnnimator?.SetBool("isOpen", true);
         }
     }
 }
